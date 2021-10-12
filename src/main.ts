e10dd9ca396ca7207748aec4b099f0a3d13de3ff
konhi <hello.konhi@gmail.com>
2021-10-12 18:02:22 +0200

    feat: +1

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import createLogger from './logger'
 const log = createLogger()
 
 const client = new Discord.Client({
-  intents: [Discord.Intents.FLAGS.GUILDS]
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]
 })
 
 client.once('ready', () => {
   log.info('Bot is ready.')
+})
+
+client.on('messageCreate', message => {
+  if (message.content === '+1' && message.channel.type) {
+    message.channel.send('Oj byczku **+1**')
+  }
 })
 
 client.login(process.env.DISCORD_TOKEN)
