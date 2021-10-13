53df6ad4ff14926bcc920412ba46c5e02eb46bae
konhi <hello.konhi@gmail.com>
2021-10-14 00:08:45 +0200

    feat: /spolecznosciowy /facebook

--- src/commands.ts
+++ src/commands.ts
@@ -22,6 +22,22 @@ export const slashCommands = [
       content: 'https://play.google.com/store/apps/details?id=pl.krystian_wybranowski.elektronPlus',
       ephemeral: true
     }
+  },
+  {
+    name: 'spolecznosciowy',
+    description: '✨ Dołącz do serwera społecznościowego szkoły',
+    reply: {
+      content: 'https://discord.gg/jrDxSTE',
+      ephemeral: true
+    }
+  },
+  {
+    name: 'facebook',
+    description: '💕 Obserwuj nas na Facebooku',
+    reply: {
+      content: 'https://www.facebook.com/zgelektronik/ & https://www.facebook.com/suelektron/',
+      ephemeral: true
+    }
   }
 ]
 
